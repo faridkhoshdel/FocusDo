1fc71e188f8c68e3a3dced68d5917254e8725d88
Farid Khoshdel <blueframeweb@gmail.com>
2026-09-18 01:57:39 +0330

    Update build.gradle.kts

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -71,5 +71,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
